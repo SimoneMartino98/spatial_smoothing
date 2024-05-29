abf5ead891da380c6fd91fe033eb55865406ae82
SimoneMartino98 <s.martino0898@gmail.com>
2024-05-29 10:10:14 +0200

    added gitignore

--- spatial_smoothing.py
+++ spatial_smoothing.py
@@ -1,16 +1,48 @@
 from __future__ import annotations
-import MDAnalysis as mda 
+import MDAnalysis as mda
 from MDAnalysis.analysis.distances import distance_array
 import numpy as np
 
 def spatial_smoothing(
         universe: mda.Universe,
+        array: np.ndarray,
         selection: str,
-        cutoff: float):
-    selection = u.select_atoms(selection)
-    for ts in universe.trajectory:
-        distances = distance_array(selection.positions, selection.positions, box=universe.dimensions)
-        print(distances)
+        cutoff: float) -> np.ndarray:
+    selection = universe.select_atoms(selection)
+    array = np.load(array)
 
-u = mda.Universe("test/ice_water.gro", "test/ice_water_500.xtc")
-spatial_smoothing(u,"type O",10.0)
+    if (array.ndim == 2):
+        sp_array = np.zeros((array.shape[0], array.shape[1]))
+        vector = False
+    elif (array.ndim == 3):
+        sp_array = np.zeros((array.shape[0], array.shape[1], array.shape[2]))
+        vector = True
+    else:
+        print("INVALID ARRAY SHAPE")
+
+    for ts in universe.trajectory:
+        frame = ts.frame
+        print(f"FRAME: {frame}")
+        distances = distance_array(selection.positions, selection.positions, box=universe.dimensions)
+        atom_id = np.argsort(distances, axis=1)
+        nn = np.sum(distances < cutoff, axis=1)
+        
+        rows = np.arange(distances.shape[0])
+        sp_dict = {row: atom_id[row, :nn[row]] for row in rows}
+
+        for key, value in sp_dict.items():
+            if(vector):
+                sp_array[key,frame,:] = np.mean(array[value, frame,:],axis=0)
+            else:
+                sp_array[key,frame] = np.mean(array[value, frame])
+
+    return sp_array
+        
+
+# EXAMPLE
+# cutoff = 10
+# array = "LENS_10.npy"
+# u = mda.Universe("test/ice_water.gro", "test/ice_water_500.xtc")
+# sp_array = spatial_smoothing(u, array, "type O", cutoff)
+# np.save(f"sp_{cutoff}_{array}", sp_array)
+
